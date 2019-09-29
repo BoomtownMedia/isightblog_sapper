fdf51be8b5124bb56392c88d5c703aba42118af8
Adam Falck <adam.falck@gmail.com>
2019-09-28 22:29:47 -0500

    first commit

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,34 +1,21 @@
-<script context="module">
-	export function preload({ params, query }) {
-		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-			return { posts };
-		});
-	}
-</script>
-
 <script>
-	export let posts;
-</script>
+  import { onMount } from "svelte";
+  let posts = [];
 
-<style>
-	ul {
-		margin: 0 0 1em 0;
-		line-height: 1.5;
-	}
-</style>
+  onMount(async function() {
+    const response = await fetch("https://blog-api-isight.herokuapp.com/posts");
+    posts = await response.json();
+  });
+</script>
 
 <svelte:head>
-	<title>Blog</title>
+  <title>Blog</title>
 </svelte:head>
 
-<h1>Recent posts</h1>
-
 <ul>
-	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
-		<li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li>
-	{/each}
+  {#each posts as post}
+    <li>
+      <a rel="prefetch" href="blog/{post.slug}">{post.title}</a>
+    </li>
+  {/each}
 </ul>
