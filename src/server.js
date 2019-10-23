cee3751a2d59311a1457e41c88ee5d75f4ce7318
Adam Falck <adam.falck@gmail.com>
2019-10-23 16:50:54 -0500

    requirehttps added

--- src/server.js
+++ src/server.js
@@ -5,9 +5,21 @@ import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (
+    !req.secure &&
+    req.get("x-forwarded-proto") !== "https" &&
+    process.env.NODE_ENV !== "development"
+  ) {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
 
 express()
   .use(
+    requireHTTPS,
     compression({ threshold: 0 }),
     sirv("static", { dev }),
     sapper.middleware()
